d56831fd4b1ca5c93f5e277d02148af029e19b83
maxtang2013 <yllfever@gmail.com>
2017-03-13 16:14:57 +0800

    Hello world iOS app with swift

---
+++ FoodTraker/FoodTraker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTraker
+//
+//  Created by max tang on 17/3/13.
+//  Copyright © 2017年 max tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
